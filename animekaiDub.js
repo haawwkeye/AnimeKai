9b30171a8255f9e902ca7d71e1381372acdf3b1c
haawwkeye <connorbishop53@gmail.com>
2025-05-12 13:35:35 -0400

    fix bugs! (hopefully)

--- animekaiDub.js
+++ animekaiDub.js
@@ -3,14 +3,32 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 function fetchv3(url, options = {}) {
-	return fetch(url, options) /*.then((response => {
-        console.log(response) // Log all responses so I can debug
-        
-        if (!response.ok)
-            throw new Error(`Network response was not ok: ${response.statusText} (${response.status})`);
-        return response
-    }
-    ))*/
+	return fetch(url, options)
+		.then((response) => {
+			if (typeof response === "string") {
+				return {
+					text: () => response,
+					json: () => {
+						try {
+							return JSON.parse(response);
+						} catch (error) {
+							console.error("JSON parse error:", error);
+							throw new Error(`Failed to parse JSON response ${error}`);
+						}
+					},
+				};
+			} else if (response instanceof Response) {
+				if (!response.ok)
+					throw new Error(
+						`Network response was not ok: ${response.statusText} (${response.status})`
+					);
+				return response;
+			} else {
+				throw new Error(
+					`Invalid response type: ${typeof response} (${response})`
+				);
+			}
+		})
 		.catch((error) => {
 			throw (
 				(console.error("Fetch error:", error),
---
+++ fetchv3.js
@@ -0,0 +1,38 @@
+// My custom fetch function since v2 doesn't work for some reason?
+// (this is a copy of the v2 function, but with some changes to make it work)
+
+function fetchv3(url, options = {}) {
+	return fetch(url, options)
+		.then((response) => {
+			if (typeof response === "string") {
+				return {
+					text: () => response,
+					json: () => {
+						try {
+							return JSON.parse(response);
+						} catch (error) {
+							console.error("JSON parse error:", error);
+							throw new Error(`Failed to parse JSON response ${error}`);
+						}
+					},
+				};
+			} else if (response instanceof Response) {
+				if (!response.ok)
+					throw new Error(
+						`Network response was not ok: ${response.statusText} (${response.status})`
+					);
+				return response;
+			} else {
+				throw new Error(
+					`Invalid response type: ${typeof response} (${response})`
+				);
+			}
+		})
+		.catch((error) => {
+			throw (
+				(console.error("Fetch error:", error),
+				showMessage("Fetch Error", "error", error.message),
+				error)
+			);
+		});
+}
